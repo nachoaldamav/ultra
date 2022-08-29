7d30b57e141422a394ffed4bad5e3e67cf639979
nachoaldamav <sr.drabx@gmail.com>
2022-08-29 22:04:25 +0200

    refactor 🔨: (cli) New install logic using NPM Arborist

--- packages/cli/src/commands/install.ts
+++ packages/cli/src/commands/install.ts
@@ -9,7 +9,11 @@ import { compareSemanticVersions } from "../utils/sortVersions.js";
 import { existsSync } from "fs";
 import Arborist from "@npmcli/arborist";
 
-const arb = new Arborist();
+const options = {
+  registry: "https://snpm-edge.snpm.workers.dev/package/",
+};
+
+const arb = new Arborist(options);
 
 export let depsArray: {
   name: string;
@@ -21,176 +25,19 @@ export let depsArray: {
 }[] = [];
 
 export async function install(packages: string[]) {
-  const loadingPackages = ora(
-    chalk.green(`Installing packages: ${packages.join(", ")}...`)
-  ).start();
-
-  const isLocalInstall = packages.length === 0;
-
-  // Remove node_modules/.bin folder if it exists
-  const binPath = path.join(process.cwd(), "node_modules", ".bin");
-  if (existsSync(binPath)) {
-    await rm(binPath, { recursive: true });
-  }
-
-  // If no packages are passed, install dependencies from package.json
-  if (packages.length === 0) {
-    const packageJSON = await readFile(
-      path.join(process.cwd(), "package.json"),
-      "utf8"
-    );
-
-    const packageJSONObject = JSON.parse(packageJSON);
-    const dependencies = packageJSONObject.dependencies;
-    const devDependencies = packageJSONObject?.devDependencies || {};
-
-    // Create array of dependencies to install with name and version
-    const allDependencies = [
-      ...Object.keys(dependencies).map((key) => `${key}@${dependencies[key]}`),
-      ...Object.keys(devDependencies).map(
-        (key) => `${key}@${devDependencies[key]}`
-      ),
-    ];
-
-    // Sort deps by alphabetical order
-    packages = allDependencies.sort();
-  }
-
-  ora(
-    chalk.blue(`Fetching dependencies..., ${JSON.stringify(packages, null, 0)}`)
-  ).info();
-
-  loadingPackages.succeed();
-
-  const fetchingPackages = ora(chalk.green("Fetching packages...")).start();
-
-  // Fetch packages from npm registry
-  const promises = packages.map((packageName) =>
-    getAllDependencies(packageName)
-  );
-
-  await Promise.all(promises);
-
-  fetchingPackages.text = chalk.green("All packages fetched!");
-  fetchingPackages.succeed();
-
-  // Write depsArray to file
-  const writingPackages = ora(chalk.green("Writing packages...")).start();
-  const depsArrayString = JSON.stringify(depsArray, null, 2);
-  await writeFile(
-    path.join(path.join(process.cwd(), "depsArray.json")),
-    depsArrayString
-  );
-  writingPackages.succeed();
-
-  // Remove duplicates from depsArray by name, version, parent and parentVersion
-  const depsArrayNoDuplicates = depsArray.filter(
-    (dep, index) =>
-      depsArray.findIndex(
-        (dep2) =>
-          dep.name === dep2.name &&
-          dep.version === dep2.version &&
-          dep.parent === dep2.parent &&
-          dep.parentVersion === dep2.parentVersion
-      ) === index
-  );
-
-  const downloadingPackages = ora(
-    chalk.green("Downloading packages...")
-  ).start();
-
-  // Download tarballs for each package
-  Promise.all(
-    depsArrayNoDuplicates.map(async (dep) => {
-      // Get all dependencies with the same name
-      const deps = depsArrayNoDuplicates.filter((d) => d.name === dep.name);
-
-      // Get dep that doesn't have a parent
-      const isLocal = deps.find((d) => d.parent === "");
-
-      // If no parent, download package to root
-      if (isLocal && isLocal === dep) {
-        await downloadPackage(dep.tarball, dep.name);
-        return;
-      }
-
-      // Find deps with same parent, but different version
-      const sameParent = deps.filter((d) => d.parent === dep.parent);
-      const sameParentDifferentVersion = sameParent.filter(
-        (d) => d.parentVersion !== dep.parentVersion
-      );
-
-      if (sameParentDifferentVersion.length > 1) {
-        // If there are deps with same parent, but different version, download package to parent
-        ora(
-          chalk.blue(
-            `${dep.name}@${dep.version} has ${
-              sameParentDifferentVersion.length
-            } other versions of ${
-              dep.parent
-            } with versions ${sameParentDifferentVersion
-              .map((d) => d.parentVersion)
-              .join(", ")}`
-          )
-        ).info();
-      }
-
-      // If there is more than one version of a package, send latest version to root, otherwise send version to parent folder
-      const packageArray = [...new Set(deps)];
-
-      if (packageArray.length === 1) {
-        await downloadPackage(dep.tarball, dep.name, null);
-      } else if (packageArray.length > 1) {
-        const sortedVersions = packageArray.sort(compareSemanticVersions);
-        const latestVersion = sortedVersions[sortedVersions.length - 1];
-        const isLatestVersion = dep.version === latestVersion.version;
-
-        await downloadPackage(
-          dep.tarball,
-          dep.name,
-          isLatestVersion && !isLocal ? null : dep.parent
-        ).then(() => {
-          downloadingPackages.text = chalk.green(`${dep.name} installed...`);
-        });
-      } else {
-        await downloadPackage(dep.tarball, dep.name)
-          .then(() => {
-            downloadingPackages.text = chalk.green(`${dep.name} installed...`);
-          })
-          .catch((error) => {});
-      }
+  const spinner = ora("Generating tree...").start();
+  await arb.buildIdealTree({
+    add: packages,
+  });
+  spinner.succeed();
+  const spinner2 = ora("Downloading packages...").start();
+  await arb
+    .reify({
+      save: true,
     })
-  )
-    .then(async () => {
-      // Add dependencies to package.json
-      const packageJSON = await readFile(
-        path.join(process.cwd(), "package.json"),
-        "utf8"
-      );
-
-      const packageJSONObject = JSON.parse(packageJSON);
-      const dependencies = packageJSONObject.dependencies;
-
-      // Add packages to dependencies
-      if (!isLocalInstall) {
-        packages.forEach((p) => {
-          const dep = depsArrayNoDuplicates.find((d) => d.name === p);
-          if (dep) {
-            dependencies[dep?.name] = dep?.version;
-          }
-        });
-
-        await writeFile(
-          path.join(process.cwd(), "package.json"),
-          JSON.stringify(packageJSONObject, null, 2),
-          "utf8"
-        );
-      }
-    })
-    .finally(async () => {
-      downloadingPackages.succeed(
-        chalk.green(`${depsArray.length} packages installed!`)
-      );
+    .then(() => {
+      spinner2.text = "Packages downloaded!";
+      spinner2.succeed();
     });
 }
 
