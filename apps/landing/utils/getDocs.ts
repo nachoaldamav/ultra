7bc2d71eb4c2c4a1fbd7fb0e9da73d2f29e895ea
nachoaldamav <sr.drabx@gmail.com>
2022-09-12 19:37:45 +0200

    fix 🐛: (landing) Fix Docs routes in prod

--- apps/landing/utils/getDocs.ts
+++ apps/landing/utils/getDocs.ts
@@ -2,21 +2,21 @@ import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
 
+// Path to the docs directory
+const docsPath = path.join(process.cwd(), "app", "routes", "docs");
+
 export async function getDocs() {
-  const postsPath = await fs.readdir(
-    path.join(`${__dirname}`, "/../", "app/routes/docs"),
-    {
-      withFileTypes: true,
-    }
-  );
+  console.log("getDocs");
+  console.log(docsPath);
+  const postsPath = await fs.readdir(docsPath, {
+    withFileTypes: true,
+  });
 
   const posts = await Promise.all(
     postsPath
       .filter((post) => post.name.endsWith(".mdx"))
       .map(async (dirent) => {
-        const file = await fs.readFile(
-          path.join(`${__dirname}`, "/../", "app/routes/docs", dirent.name)
-        );
+        const file = await fs.readFile(path.join(docsPath, dirent.name));
 
         const { attributes }: { attributes: DocsInfo } = parseFrontMatter(
           file.toString()
