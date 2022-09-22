cb68d68bafc9a343b457c5cfdf4b37880ec3e318
nachoaldamav <sr.drabx@gmail.com>
2022-09-22 19:01:31 +0200

    feat ✨: (cli) Check if correct Node version is used

---
+++ packages/cli/src/utils/checkNodeVersion.ts
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+import ora from "ora";
+import { satisfies } from "compare-versions";
+
+export default async function checkNodeVersion(engines: any) {
+  const { node } = engines;
+  const currentVersion = process.version;
+  const requiredVersion = node;
+
+  if (!satisfies(currentVersion, requiredVersion)) {
+    ora().warn(
+      chalk.yellow(
+        `Node version ${currentVersion} does not satisfy required version ${requiredVersion}`
+      )
+    );
+  }
+}
