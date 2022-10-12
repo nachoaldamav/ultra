9bffa51cb4950c4d5c16d316d48be790a5311e51
nachoaldamav <sr.drabx@gmail.com>
2022-10-12 19:39:16 +0200

    chore 🔧: (chore) Remove web folder

--- apps/web/components/navBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function NavBar() {
-  return (
-    <div className="flex flex-row justify-between items-center w-full h-16 bg-[#3B3B3B] px-10">
-      <h1 className="text-2xl font-bold text-center">SNPM.IO</h1>
-      <div className="flex flex-row justify-between items-center w-1/4 h-full"></div>
-    </div>
-  );
-}
--- apps/web/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-import AppLayout from '../components/appLayout';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <AppLayout><Component {...pageProps} /></AppLayout>
-}
-
-export default MyApp;
