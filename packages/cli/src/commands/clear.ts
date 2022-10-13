840ed88e9f198e4a3be04477e3524c5836d14ae6
nachoaldamav <sr.drabx@gmail.com>
2022-10-13 19:23:31 +0200

    fix 🐛: (cli) Fix clear cache command

--- packages/cli/src/commands/clear.ts
+++ packages/cli/src/commands/clear.ts
@@ -8,7 +8,7 @@ import readPackage from "../utils/readPackage.js";
 export async function clear() {
   const cacheFolder = `${os.homedir()}/.ultra-cache`;
   const packageJson = `${process.cwd()}/package.json`;
-  const tmpCacheFolder = path.join(os.tmpdir(), "ultra-cache");
+  const tmpCacheFolder = path.join(os.tmpdir(), "ultra_tmp");
   const manifestsFolder = path.join(os.homedir(), ".ultra", "__manifests__");
   const pkg = readPackage(packageJson);
   const workspaces = pkg.workspaces || null;
