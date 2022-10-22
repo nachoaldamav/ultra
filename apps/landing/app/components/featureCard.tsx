f1588620ec51e74498b3a28e19c28292c952dd09
nachoaldamav <sr.drabx@gmail.com>
2022-10-22 13:44:48 +0200

    docs 📝: (landing) Update landing style

--- apps/landing/app/components/featureCard.tsx
+++ apps/landing/app/components/featureCard.tsx
@@ -6,11 +6,9 @@ export default function Feature({
   description: string;
 }) {
   return (
-    <article className="flex h-auto w-4/5 mx-auto md:mx-0 md:h-32 md:w-1/3 flex-col items-center justify-center gap-4 rounded-lg border border-gray-400 bg-secondary bg-transparent p-4 transition duration-150 ease-in-out hover:border-transparent hover:bg-secondary">
-      <h1 className="text-2xl font-bold text-white">{title}</h1>
-      <p className="text-center text-lg font-extralight text-white">
-        {description}
-      </p>
+    <article className="flex h-auto w-3/5 mx-auto md:mx-0 md:h-32 md:w-1/4 flex-col items-center justify-start gap-2 bg-transparent p-4">
+      <h1 className="text-xl font-bold text-white">{title}</h1>
+      <p className="text-center text-base text-[#71717A]">{description}</p>
     </article>
   );
 }
